ef033544671fffbb9f14c20e9ebb15dca59737b2
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-06 15:42:31 +0200

    load data and pass them to job cards components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
-  ciao
+  <div>
+    <job-card v-for="job in jobs" :job="job" :key="job.id"></job-card>
+  </div>
 </template>
 
 <script>
-
+import data from "@/json/data.json";
+import JobCard from "@/components/JobCard";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      jobs: data,
+    };
+  },
   components: {
- 
-  }
-}
+    JobCard
+  },
+};
 </script>
 
 <style>
-
 </style>

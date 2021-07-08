4584b177515a92a453228e1d664a34d2024eecdd
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-07-08 18:44:55 +0200

    search feature

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <Header></Header>
     <div class="lg:px-28 lg:py-14 py-14 px-8">
+      <Search @search="updateJobList"></Search>
       <JobCard v-for="job in jobs" :job="job" :key="job.id"></JobCard>
     </div>
   </div>
@@ -11,24 +12,52 @@
 import data from "@/json/data.json";
 import JobCard from "@/components/JobCard";
 import Header from "@/components/Header";
+import Search from "@/components/Search";
 
 export default {
   name: "App",
   data() {
     return {
       jobs: data,
+      filteredJobs: [],
+      Ids: [],
     };
   },
   components: {
     JobCard,
-    Header
+    Header,
+    Search,
+  },
+  methods: {
+    updateJobList(e) {
+      this.jobs = data;
+      this.filteredJobs = [];
+      this.Ids = [];
+      if (e == "") {
+        return;
+      } else {
+        e.trim().split(" ").forEach((word) => {
+          this.jobs.forEach((job) => {
+            if (
+              !this.Ids.includes(job.id) &&
+                (job.position.toLowerCase().includes(word.toLowerCase()) ||
+              job.role.toLowerCase().includes(word.toLowerCase()))
+            ) {
+              this.filteredJobs.push(job);
+              this.Ids.push(job.id);
+            }
+          });
+        });
+        this.jobs = this.filteredJobs;
+      }
+    },
   },
 };
 </script>
 
 <style>
 body {
-  background-color: #F0FAFB;
+  background-color: #f0fafb;
 }
 * {
   margin: 0;
---
+++ src/components/Search.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex justify-center mb-6">
+      <input v-model="searchQuery" @input="query" class="p-2 w-full shadow-xl" type="text">
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      searchQuery : ''
+    };
+  },
+  methods: {
+    query() {
+      this.$emit('search', this.searchQuery)
+    }
+  },
+};
+</script>
+
+<style scoped>
+</style>
